4f73bc11613b99ba822c37aed69b8e046a5009d9
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-07-17 07:59:00 +0530

    Update ShapeController.java

--- Assignment 3/Question 3/src/ShapeController.java
+++ Assignment 3/Question 3/src/ShapeController.java
@@ -16,76 +16,75 @@ public class ShapeController
 		double area = 0;
 		double radius = 0;
 		
-		// area of triangle	
+	// area of triangle	
     	try 
     	{
             System.out.println("Enter width of Triangle");
-		    width = sc.nextDouble();
-			System.out.println("Enter height of Triangle");
-			height = sc.nextDouble();
-		} 
+	    width = sc.nextDouble();
+            System.out.println("Enter height of Triangle");
+	    height = sc.nextDouble();
+	} 
        catch (Exception e) 
     	{
-			System.out.println(e);
-			System.out.println("Entered value is wrong");
-			System.exit(0);
-		}
+	     System.out.println(e);
+             System.out.println("Entered value is wrong");
+	     System.exit(0);
+	}
     	 area = Area.triangleOfArea(width, height);		
     	 System.out.printf("\nArea of Triangle is: %.2f", area);
     	 System.out.println();
 	
     	 // area of Rectangle
-		try
-		{
-			System.out.println("Enter width of Rectangle");
-			width = sc.nextDouble();
-			System.out.println("Enter height of Rectangle");
-			height =sc.nextDouble();
+	try
+	{
+	    System.out.println("Enter width of Rectangle");
+	    width = sc.nextDouble();
+	    System.out.println("Enter height of Rectangle");
+	    height =sc.nextDouble();
     	}
-		catch (Exception e) 
-		{
-			System.out.println(e);
-			System.out.println("Entered value is wrong");
-			System.exit(0);
-		}
+	catch (Exception e) 
+	{
+	    System.out.println(e);
+	    System.out.println("Entered value is wrong");
+	    System.exit(0);
+	}
          area = Area.rectangleOfArea(width, height);
          System.out.printf("\nArea of Rectangle is: %.2f", area);	
          System.out.println();
 		
          // Area of Circle
-		try
-		{
-			System.out.println("Enter radius of Circle");
-			radius = sc.nextDouble();
-		} 
-		catch (Exception e) 
-		{
+	try
+	{
+	    System.out.println("Enter radius of Circle");
+	    radius = sc.nextDouble();
+	} 
+	catch (Exception e) 
+	{
 		
-			System.out.println(e);
-			System.out.printf("Entered value is wrong");
-			System.exit(0);
-		}
+	    System.out.println(e);
+	    System.out.printf("Entered value is wrong");
+	    System.exit(0);
+	}
          area = Area.circleOfArea(radius);
          System.out.printf("\nArea of Circle is: %.2f", area);	
          System.out.println();
         
         // Area of Square  
-		try 
-		{
-			System.out.println("\nEnter width of Square");
-			width = sc.nextDouble();
-			
-		} 
-		catch (Exception e) 
-		{
-			System.out.println(e);
-			System.out.println("Entered value is wrong");
-			System.exit(0);
-		}
-        area = Area.squareOfArea(width);
-		System.out.printf("\nArea of Square is: %.2f", area);
-		sc.close();
+	try 
+	{
+	    System.out.println("\nEnter width of Square");
+	     width = sc.nextDouble();
+	} 
+	catch (Exception e) 
+	{
+	    System.out.println(e);
+	    System.out.println("Entered value is wrong");
+	    System.exit(0);
 	}
+        area = Area.squareOfArea(width);
+	System.out.printf("\nArea of Square is: %.2f", area);
+	sc.close();
+   }
 }
 
 
