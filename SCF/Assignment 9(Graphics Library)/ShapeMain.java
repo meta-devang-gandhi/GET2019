b7a06ca2d7f9cc05e28b4f895353b0693170ee02
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-07-29 11:04:33 +0530

    Update ShapeMain.java

--- SCF/Assignment 9(Graphics Library)/ShapeMain.java
+++ SCF/Assignment 9(Graphics Library)/ShapeMain.java
@@ -13,6 +13,7 @@ public class ShapeMain
         Screen screen = new Screen();
         Scanner scanner = new Scanner(System.in);
         int operationChoice = 0;
+        boolean flag = true;
         while (operationChoice < 6) 
         {
             Shape shape = null;
@@ -31,7 +32,6 @@ public class ShapeMain
                         System.out.println(value.name());
                     }
 
-
                     String choice = scanner.next();
                     ShapeType shapeSelected = null;
                     for (ShapeType value : ShapeType.values()) 
@@ -46,56 +46,151 @@ public class ShapeMain
                         System.out.println("Invalid shape entered !");
                         break;
                     }
-
-                    System.out.println("Enter origin coordinate:");
-                    System.out.println("X= ");
-                    int originX = scanner.nextInt();
-                    System.out.println("Y= ");
-                    int originY = scanner.nextInt();
-
+                     
+                    try
+                    {
+                        flag = true;
+                        while(flag)
+                        {
+                             System.out.println("Enter origin coordinate:");
+                             System.out.println("X= ");
+                             int originX = scanner.nextInt();
+                             System.out.println("Y= ");
+                             int originY = scanner.nextInt();
+                            
+                            if(originX < 0 || originY < 0)
+                            {
+                                System.out.println("Your input is negative, please enter positive vlaue of origin coordinate ");
+                            }
+                            else
+                            {
+                                flag = false;
+                            }
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
                     Point point = new Point(originX, originY);
                     parametersList.removeAll(parametersList);
                     switch (shapeSelected) 
                     {
                         case RECTANGLE:
                         {
-                            System.out.println("Enter the length: ");
-                            int length = scanner.nextInt();
-                            parametersList.add(length);
-                            System.out.println("Enter the width: ");
-                            int width = scanner.nextInt();
-                            parametersList.add(width);
-
+                            try
+                            {
+                               flag = true;
+                               while(flag)
+                               {
+                                   System.out.println("Enter the length: ");
+                                   int length = scanner.nextInt();
+                                   parametersList.add(length);
+                                   System.out.println("Enter the width: ");
+                                   int width = scanner.nextInt();
+                                   parametersList.add(width);
+                                   
+                                   if(length < 0 || width < 0)
+                                   {
+                                       System.out.println("Your input is negative, please again enter rectangle positive length and width");
+                                   }
+                                   else
+                                   {
+                                       flag = false;
+                                   }
+                               }
+                            }
+                            catch(Exception e)
+                            {
+                               System.out.println(e);
+                            }
                             break;
                         }
                         case SQUARE:
                         {
-                            System.out.println("Enter the side: ");
-                            int side = scanner.nextInt();
-                            parametersList.add(side);
-
+                            try
+                            {
+                                flag = true;
+                                while(flag)
+                                {
+                                    System.out.println("Enter the side: ");
+                                    int side = scanner.nextInt();
+                                     parametersList.add(side);
+                                    
+                                    if(side < 0)
+                                    {
+                                         System.out.println("Your input is negative, please again enter square positive side");
+                                    }
+                                    else
+                                    {
+                                       flag = false;
+                                    }
+                                }
+                            }
+                            catch(Exception e)
+                            {
+                               System.out.println(e);
+                            }
                             break;
                         }
                         case CIRCLE:
                         {
-                            System.out.println("Enter the radius: ");
-                            int radius = scanner.nextInt();
-                            parametersList.add(radius);
-
+                            try
+                            {
+                                flag = true;
+                                while(flag)
+                                {
+                                    System.out.println("Enter the radius: ");
+                                    int radius = scanner.nextInt();
+                                    parametersList.add(radius);
+                                    
+                                    if(radius < 0)
+                                    {
+                                        System.out.println("Your input is negative, please again enter circle positive radius");
+                                    }
+                                    else 
+                                    {
+                                      flag = false;
+                                    }
+                                }
+                            }
+                            catch(Exception e)
+                            {
+                               System.out.println(e);
+                            }
                             break;
                         }
                         case TRIANGLE:
                         {
-                            System.out.println("Enter the side1: ");
-                            int side1 = scanner.nextInt();
-                            parametersList.add(side1);
-                            System.out.println("Enter the side2: ");
-                            int side2 = scanner.nextInt();
-                            parametersList.add(side2);
-                            System.out.println("Enter the side3: ");
-                            int side3 = scanner.nextInt();
-                            parametersList.add(side3);
-
+                            try
+                            {
+                                flag = true;
+                                while(flag)
+                                {
+                                   System.out.println("Enter the side1: ");
+                                   int side1 = scanner.nextInt();
+                                   parametersList.add(side1);
+                                   System.out.println("Enter the side2: ");
+                                    int side2 = scanner.nextInt();
+                                   parametersList.add(side2);
+                                    System.out.println("Enter the side3: ");
+                                    int side3 = scanner.nextInt();
+                                    parametersList.add(side3);
+                                    
+                                    if(side1 < 0 || side2 <0 || side3 <0)
+                                    {
+                                        System.out.println("Your input is negative, please again enter tringle positive side");
+                                    }
+                                    else
+                                    {
+                                        flag = false;
+                                    }
+                                }
+                            }
+                            catch(Exception e)
+                            {
+                               System.out.println(e);
+                            }
                             break;
                         }
                         default:
@@ -135,11 +230,17 @@ public class ShapeMain
                 }
                 case 5:
                 {
-                    System.out.println("Enter x coordinate: ");
-                    int xCoordinate = scanner.nextInt();
-                    System.out.println("Enter Y coordinate: ");
-                    int yCoordinate = scanner.nextInt();
-
+                    try
+                    {
+                       System.out.println("Enter x coordinate: ");
+                       int xCoordinate = scanner.nextInt();
+                       System.out.println("Enter Y coordinate: ");
+                        int yCoordinate = scanner.nextInt();
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
                     Point point1 = new Point(xCoordinate, yCoordinate);
                     for (Shape value: screen.getListOfShapesEnclosingThePoint(point1)) 
                     {
