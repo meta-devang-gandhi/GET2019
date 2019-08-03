68b7358d99d17c5a79be4212505453e58e19eca6
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-08-03 22:50:13 +0530

    Update UniqueCharacter.java

--- DS-Session 4/question2/UniqueCharacter.java
+++ DS-Session 4/question2/UniqueCharacter.java
@@ -1,65 +1,86 @@
 package question2;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *Class for count unique character from string.
  */
 public class UniqueCharacter
 {
-	Map<String, Integer> uniqueChar = new HashMap<String, Integer>();
-
-	/**
-	 * Count the unique character from string.
-	 * @param string
-	 * @return number of unique character.
-	 */
-	public int countUniqueCharacter(String string) 
-	{	
-		string = string.toLowerCase();
-		
-		int noOfUniqueChar = 0;
-		char currentChar = 0;
-
-		// if we already have string on map then we direct get the number of unique char and return it.
-		if (uniqueChar.containsKey(string)) 
-		{
-			return uniqueChar.get(string);
-		} 
-		else 
-		{
-			Map<Character, Integer> uniqueCharMap = new HashMap<Character, Integer>();
-			
-			try
-			{
-				for (int currentCharIndex = 0; currentCharIndex < string.length(); currentCharIndex++) 
-				{
-					currentChar = string.charAt(currentCharIndex);
-					uniqueCharMap.put(currentChar, uniqueCharMap.getOrDefault(currentChar, 0) + 1);
-				}
+	private Map<String,Integer> cacheMap;
 	
-				for (Character currentCharKey : uniqueCharMap.keySet()) 
+	void setCache(Map<String, Integer> cacheMap) 
+	{
+		this.cacheMap = cacheMap;
+	}
+	
+	UniqueCharacter()
+	{	
+		this.cacheMap = new HashMap<String, Integer>();
+	}
+	
+	/**
+	 *Counts the number of unique characters in a string
+	 * @param inputString is to be checked.
+	 * @return numbers of unique character. 
+	 */
+	int countUniqueCharacters(String inputString) 
+	{	
+		Set<Character> uniqueCharSet = new HashSet<Character>();
+		try
+		{
+			if(inputString == null || inputString.equals(" "))
+			{	
+				throw new Exception("input string is invalid");	
+			}
+			else
+			{
+				if(cacheMap.containsKey(inputString))
 				{
-					if (uniqueCharMap.get(currentCharKey) == 1) 
-					{
-						noOfUniqueChar++;
+					return cacheMap.get(inputString);
+				}
+				else
+				{	
+					inputString = inputString.toLowerCase();
+					
+					int length = inputString.length();
+					for(int index = 0; index < length; index++)
+					{	
+						char charAtIndex = inputString.charAt(index);
+						
+						if(charAtIndex == ' ')
+						{
+							continue;
+						}
+						else
+						{
+							if(uniqueCharSet.contains(charAtIndex) == false)
+							{
+								uniqueCharSet.add(charAtIndex);
+							}
+						}
 					}
 				}
 			}
-			catch(Exception e)
-			{
-				System.out.println(e);
-			}
-			uniqueChar.put(string, noOfUniqueChar);
-			return noOfUniqueChar;
+			
+			cacheMap.put(inputString, uniqueCharSet.size());
+			return uniqueCharSet.size();
 		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		return 0;
 	}
 	
 	public static void main(String[] args) 
 	{
 		UniqueCharacter uniqueCharacterInString = new UniqueCharacter ();
-		System.out.println(  uniqueCharacterInString.countUniqueCharacter("devannngg gggggadhi"));
-		System.out.println(  uniqueCharacterInString.countUniqueCharacter("devannngg gggggadhi"));
+		System.out.println(  uniqueCharacterInString.countUniqueCharacters("devannngg gggggadhi"));
+		System.out.println(  uniqueCharacterInString.countUniqueCharacters("devannngg gggggadhi"));
 	}
 }
