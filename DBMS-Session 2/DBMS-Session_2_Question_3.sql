8206996500f4ab5939cb60fbb319cdbabf86076b
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-08-13 19:51:40 +0530

    Add files via upload

---
+++ DBMS-Session 2/DBMS-Session_2_Question_3.sql
@@ -0,0 +1,11 @@
+#1
+SELECT OrderId, OrderDate, OrderDate OrderAmount from Orders 
+ORDER BY OrderId DESC
+LIMIT 50;
+
+#2
+SELECT OrderId, OrderDate, OrderDate OrderAmount from Orders 
+ORDER BY OrderAmount DESC
+LIMIT 10;
+
+#3
