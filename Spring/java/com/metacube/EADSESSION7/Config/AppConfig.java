f44de9a5261eee31a2cc5ffdb440a2c05f663264
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-09-07 15:10:11 +0530

    Update AppConfig.java

--- Spring/java/com/metacube/EADSESSION7/Config/AppConfig.java
+++ Spring/java/com/metacube/EADSESSION7/Config/AppConfig.java
@@ -9,6 +9,10 @@ import com.metacube.EADSESSION7.Service.IMailSender;
 import com.metacube.EADSESSION7.Service.Implimentation.MockMailSender;
 import com.metacube.EADSESSION7.Service.Implimentation.SmtpMailSender;
 
+/**
+* It is a config class.
+* Which is return the object of classes. 
+**/
 //@Configuration
 @Component
 public class AppConfig {
