16fc52b296ddf150cb5f5ddada4ff905ebacff04
meta-devang-gandhi <52689627+meta-devang-gandhi@users.noreply.github.com>
2019-09-07 15:15:06 +0530

    Update IMailSender.java

--- Spring/java/com/metacube/EADSESSION7/Service/IMailSender.java
+++ Spring/java/com/metacube/EADSESSION7/Service/IMailSender.java
@@ -4,5 +4,6 @@ package com.metacube.EADSESSION7.Service;
  * Represent the mail sender.
  */
 public interface IMailSender {
-		public String sendMail();
+     //Method to send mail.
+     public String sendMail();
 }
